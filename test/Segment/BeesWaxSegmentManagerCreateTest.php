bdf431c996813f4ee7b3d44cab5a0f12290d5f35
Giacomo Mr. Wolf Furlan <giacomo.furlan@fastwebnet.it>
2018-10-08 16:56:53 +0200

    [Segment manager] Added some more coverage

--- test/Segment/BeesWaxSegmentManagerCreateTest.php
+++ test/Segment/BeesWaxSegmentManagerCreateTest.php
@@ -2,6 +2,7 @@
 
 namespace Audiens\Test\BeesWax\Segment;
 
+use Audiens\BeesWax\Exception\BeesWaxGenericException;
 use Audiens\BeesWax\Segment\BeesWaxSegment;
 use Audiens\BeesWax\Segment\BeesWaxSegmentManager;
 use Audiens\Test\BeesWax\AbstractBeesWaxTestCase;
@@ -43,5 +44,21 @@ class BeesWaxSegmentManagerCreateTest extends AbstractBeesWaxTestCase
         $createdSegment = $manager->create($segment);
 
         TestCase::assertNotEmpty($createdSegment->getId());
+    }
+
+    public function testCreateAlreadyExistingSegmentWillFail(): void
+    {
+        $session = $this->getSession();
+        $session->login(true);
+
+        $manager = new BeesWaxSegmentManager($session);
+
+        $segment = new BeesWaxSegment(Uuid::uuid4()->toString());
+        $segment->setId('123');
+
+        $this->expectException(BeesWaxGenericException::class);
+        $this->expectExceptionCode(BeesWaxGenericException::CODE_SEGMENT_ALREADY_CREATED);
+
+        $manager->create($segment);
     }
 }
